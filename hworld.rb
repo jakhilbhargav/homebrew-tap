95d5d409c1319f59d18bbf562bfd1aab8e3ed7e7
Akhil Bhargav Josyabhatla <14699437+jakhilbhargav@users.noreply.github.com>
2019-04-30 20:48:35 -0700

    changes-1.0

--- hworld.rb
+++ hworld.rb
@@ -1,7 +1,7 @@
 class Hworld < Formula
   desc "Simple hello world script."
-  homepage "https://github.com/rob-dalton/hworld"
-  url "https://github.com/rob-dalton/hworld/raw/master/hworld-1.0.tar.gz"
+  homepage "https://github.com/jakhilbhargav/hworld"
+  url "https://github.com/jakhilbhargav/hworld/hworld-1.0.tar.gz"
   sha256 "8443118e257c4c109332ae58df932da99f3bd1291a67b8a8a0283f529bc4f48e"
   version "1.0"
 
